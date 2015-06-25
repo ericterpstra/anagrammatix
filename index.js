660d6c4c27cd3dba300b999de2037cca53a8916f
Andrew Yang <ayang14@illinois.edu>
2015-06-24 20:36:02 -0500

    Make compatible with Heroku

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.configure(function() {
 });
 
 // Create a Node.js based http server on port 8080
-var server = require('http').createServer(app).listen(8080);
+var server = require('http').createServer(app).listen(process.env.PORT || 8080);
 
 // Create a Socket.IO server and attach it to the http server
 var io = require('socket.io').listen(server);
